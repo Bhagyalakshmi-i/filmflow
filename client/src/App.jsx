56f5bb945fe018c482e5876fd15912ae783be2cc
Bhagyalakshmi K Babu <bhagyalakshmik.2024@lpu.in>
2026-07-11 19:24:58 +0530

    Set up React project and create Navbar component

---
+++ client/src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <>
+      <Navbar />
+
+      <div className="container">
+        <h1>FilmFlow</h1>
+
+        <h2>Film Production Management System</h2>
+
+        <p>Welcome to FilmFlow.</p>
+
+        <button>Get Started</button>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+    return (
+      <nav>
+        <h2>🎬 FilmFlow</h2>
+      </nav>
+    )
+  }
+  
+  export default Navbar
