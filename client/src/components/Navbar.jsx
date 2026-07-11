bf70e8e0109d9a05e18cf8fac86b8ac4dba0b80b
Bhagyalakshmi K Babu <bhagyalakshmik.2024@lpu.in>
2026-07-11 19:40:46 +0530

    Build FilmFlow navigation bar

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,7 +1,17 @@
 function Navbar() {
     return (
-      <nav>
-        <h2>🎬 FilmFlow</h2>
+      <nav className="navbar">
+        <h2 className="logo">🎬 FilmFlow</h2>
+  
+        <ul className="nav-links">
+  <li><a href="#">Home</a></li>
+  <li><a href="#">Features</a></li>
+  <li><a href="#">About</a></li>
+  <li><a href="#">Contact</a></li>
+  <li><a href="#">Login</a></li>
+        </ul>
+  
+        <button className="nav-btn">Get Started</button>
       </nav>
     )
   }
